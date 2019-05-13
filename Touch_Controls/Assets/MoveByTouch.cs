843c576e7805cbe9d833e19794c88d8b41758821
rpaano <50399801+rpaano0804@users.noreply.github.com>
2019-05-13 16:18:00 +0800

    Added Touch

---
+++ Touch_Controls/Assets/MoveByTouch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByTouch : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            
+            if(touchPosition.x > 0)
+            {
+                transform.position = touchPosition;
+            }
+
+            
+        }
+    }
+}
